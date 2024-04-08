675b165d5a602ac002fcea542056583cefbe146f
Eadweard <804875728@qq.com>
2024-04-08 13:49:21 +0800

    尝试

--- src/core/zip.rs
+++ src/core/zip.rs
@@ -1,9 +1,18 @@
+use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
+use std::io::{Read, Write};
+
+trait Zipper {
+    fn compression() {
+        let data = "hello, world";
+        let mut encoder = GzEncoder::new(Vec::new(), flate2::Compression::default());
+    }
+}
 
 #[derive(Debug)]
 pub struct Zip {
-  pub file_path: String,
-  pub target_name: String,
+    pub zip_path: String,    // 要压缩的文件的路径
+    pub target_name: String, // 压缩后的文件名
 }
 
-
-
+impl Zip {}
